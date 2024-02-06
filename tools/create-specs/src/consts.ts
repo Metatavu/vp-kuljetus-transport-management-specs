f6f23faee0129554210ad090d4478e8bcff08608
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2024-02-06 23:09:46 +0200

    Added security schemes and specs to all endpoints

--- tools/create-specs/src/consts.ts
+++ tools/create-specs/src/consts.ts
@@ -9,18 +9,25 @@ export const OPENAPI_VERSION = "3.0.3";
 export const TYK_IMAGE = "docker.tyk.io/tyk-gateway/tyk-gateway:v5.1.0";
 
 /**
+ * User roles for bearer auth
+ */
+export const USER_ROLES = ["driver", "manager"];
+
+/**
  * Template for the spec
  */
 export const SPEC_TEMPLATE = {
-  "openapi": OPENAPI_VERSION,
-  "info": {
-    "title": "Vehicle Management Services",
-    "description": "Vehicle Management Services",
-    "version": "1.0.0"
+  openapi: OPENAPI_VERSION,
+  info: {
+    title: "Vehicle Management Services",
+    description: "Vehicle Management Services",
+    version: "1.0.0"
   },
-  "paths": {},
-  "components": {
-    "schemas": {}
+  security: <any>[],
+  paths: {},
+  components: {
+    securitySchemes: {},
+    schemas: {}
   }
 };
 
@@ -29,24 +36,28 @@ export const SPEC_TEMPLATE = {
  */
 export const SPEC_VERSIONS = [
   {
-    "name": "full",
-    "description": "Full",
-    "tags": ["App", "ManagementUI", "VehicleDataReceiver"]
+    name: "full",
+    description: "Full",
+    tags: ["App", "ManagementUI", "VehicleDataReceiver"],
+    securitySchemes: ["bearerAuth", "apiKeyAuth"]
   },
   {
-    "name": "app",
-    "description": "App",
-    "tags": ["App"]
+    name: "app",
+    description: "App",
+    tags: ["App"],
+    securitySchemes: ["bearerAuth"]
   },
   {
-    "name": "management",
-    "description": "Management",
-    "tags": ["ManagementUI"]
+    name: "management",
+    description: "Management",
+    tags: ["ManagementUI"],
+    securitySchemes: ["bearerAuth"]
   },
   {
-    "name": "vehicle-data-receiver",
-    "description": "Vehicle data receiver",
-    "tags": ["VehicleDataReceiver"]
+    name: "vehicle-data-receiver",
+    description: "Vehicle data receiver",
+    tags: ["VehicleDataReceiver"],
+    securitySchemes: ["apiKeyAuth"]
   }
 ];
 
